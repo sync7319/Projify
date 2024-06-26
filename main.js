f5df7676df507c36caab0a3f13e53c286fb7c2a9
Om <omarv321@gmail.com>
2024-06-26 13:17:59 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+document.getElementById("dashboard-link").addEventListener("click", function() {
+    window.location.href = "index.html";
+});
+
+document.getElementById("projecthub-link").addEventListener("click", function() {
+    window.location.href = "projecthub.html";
+});
+
+document.getElementById("notification-link").addEventListener("click", function() {
+    window.location.href = "notifications.html";
+});
+
+document.getElementById("message-link").addEventListener("click", function() {
+    window.location.href = "messages.html";
+});
+
+document.getElementById("profile-link").addEventListener("click", function() {
+    window.location.href = "myprofile.html";
+});
